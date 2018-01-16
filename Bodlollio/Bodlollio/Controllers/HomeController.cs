00ad704eb334caf0c0f776297809b87fb6ae7618
Michael Blättler <Michael.Blaettler@landisgyr.com>
2018-01-16 16:47:33 +0100

    Question added to Home-Controller

--- Bodlollio/Bodlollio/Controllers/HomeController.cs
+++ Bodlollio/Bodlollio/Controllers/HomeController.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+/* TODO
+ * Fragen:
+ * 1) Warum haben Sie sich für gerade für den Hash Algorithmus (Usernamen & Passwort)
+ * entschieden?
+ * 
+ * 2) In der User-Login-Tabelle ist noch ein Feld für die IP-Adresse Reserviert. Welche Attacke lässt
+ * sich dadurch verhindern?
+ * 
+ * 3) Erklären Sie, wie diese Attacke genau funktioniert und inwiefern die Gegenmassnahmen die
+ * Attacke vereitelt?
+ * 
+ */
 namespace Bodlollio.Controllers
 {
     public class HomeController : Controller
