0749317adff2743c82f9236ed677eaadcca484b8
Michael Blättler <Michael.Blaettler@landisgyr.com>
2018-01-17 11:56:26 +0100

    Nexmo-Data-class for api. + fill in data by your own please

---
+++ Bodlollio/Bodlollio/App_Start/NexmoData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bodlollio.App_Start
+{
+    public class NexmoData
+    {
+        string api_key = "";
+        string api_secret = "";
+        string to = "";
+        string from = "";
+        string text = "";
+    }
+}
